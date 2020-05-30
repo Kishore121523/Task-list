c585eae5bada417630353d8eee2e1d0e469b92ab
KISHORE.Y <64627938+Kishore121523@users.noreply.github.com>
2020-05-30 18:31:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,162 @@
+// Define UI vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".list-group");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  //DOM load event
+  document.addEventListener("DOMContentLoaded", getTasks);
+  // Add task event
+  form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+  // Clear task event
+  clearBtn.addEventListener("click", clearTasks);
+  // Filter task events
+  filter.addEventListener("keyup", filterTasks);
+}
+
+//Get tasks from LS
+function getTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task) {
+    // Create li element
+    const li = document.createElement("li");
+    //Add class of list-group-item to created li
+    li.className = "list-group-item";
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(task));
+    // Create new link element
+    const link = document.createElement("a");
+    // Add class
+    link.className = "close";
+    // Add icon
+    link.innerHTML = '<i class="fas fa-window-close"></li>';
+    // Append link to li
+    li.appendChild(link);
+
+    // Appending li to ul
+    taskList.appendChild(li);
+  });
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  //Add class of list-group-item to created li
+  li.className = "list-group-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "close";
+  // Add icon
+  link.innerHTML = '<i class="fas fa-window-close"></li>';
+  // Append link to li
+  li.appendChild(link);
+
+  // Appending li to ul
+  taskList.appendChild(li);
+
+  // Store in local storage
+  storeTaskInLocalStorage(taskInput.value);
+
+  // Clear input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
+
+//Store task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("close")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove(); //as close has parent of a tag and a tag has parent of li element which we should remove.So two parentElement.
+
+      // remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+    }
+  }
+}
+
+// Remove from ls
+function removeTaskFromLocalStorage(taskitem) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function (task, index) {
+    if (taskitem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Clear tasks
+function clearTasks(e) {
+  // taskList.innerHTML = "";
+
+  // faster way
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+
+  // Clear from LS
+  clearTasksfromLocalStorage();
+}
+
+// Clear  task from local storage
+function clearTasksfromLocalStorage() {
+  localStorage.clear();
+}
+
+// filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll(".list-group-item").forEach(function (task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+}
